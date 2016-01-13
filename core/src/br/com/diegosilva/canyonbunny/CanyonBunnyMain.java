8ef1b4482147f1c00ecfbc7c42d8ac3ad955374f
Diego <diegosiuniube@gmail.com>
2016-01-13 15:27:13 -0200

    alteracoes

--- core/src/br/com/diegosilva/canyonbunny/CanyonBunnyMain.java
+++ core/src/br/com/diegosilva/canyonbunny/CanyonBunnyMain.java
@@ -2,6 +2,7 @@ package br.com.diegosilva.canyonbunny;
 
 import br.com.diegosilva.canyonbunny.game.WorldController;
 import br.com.diegosilva.canyonbunny.game.WorldRenderer;
+import com.badlogic.gdx.Application;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
@@ -10,15 +11,47 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class CanyonBunnyMain extends ApplicationAdapter {
 
-	private static final String TAG = CanyonBunnyMain.class.getName();
+    private static final String TAG = CanyonBunnyMain.class.getName();
 
-	private WorldController worldController;
-	private WorldRenderer worldRenderer;
+    private WorldController worldController;
+    private WorldRenderer worldRenderer;
+    private boolean paused;
 
-	@Override public void create () { }
-	@Override public void render () { }
-	@Override public void resize (int width, int height) { }
-	@Override public void pause () { }
-	@Override public void resume () { }
-	@Override public void dispose () { }
+    @Override
+    public void create() {
+        Gdx.app.setLogLevel(Application.LOG_DEBUG);
+        worldController = new WorldController();
+        worldRenderer = new WorldRenderer(worldController);
+        paused = false;
+    }
+
+    @Override
+    public void render() {
+        if(!paused){
+            worldController.update(Gdx.graphics.getDeltaTime());
+        }
+        Gdx.gl.glClearColor(100/255.0f, 149/255.0f, 237/255.0f, 255/255.0f);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        worldRenderer.render();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        worldRenderer.resize(width, height);
+    }
+
+    @Override
+    public void pause() {
+        paused = true;
+    }
+
+    @Override
+    public void resume() {
+        paused = false;
+    }
+
+    @Override
+    public void dispose() {
+        worldRenderer.dispose();
+    }
 }
