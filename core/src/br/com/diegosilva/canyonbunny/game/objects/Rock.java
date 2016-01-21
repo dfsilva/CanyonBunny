b208968070d06a1e04acc01cfcc2ad2118637247
Diego Silva <diegosiuniube@gmail.com>
2016-01-20 22:12:30 -0200

    alteracoes

--- core/src/br/com/diegosilva/canyonbunny/game/objects/Rock.java
+++ core/src/br/com/diegosilva/canyonbunny/game/objects/Rock.java
@@ -30,6 +30,30 @@ public class Rock extends AbstractGameObject{
     @Override
     public void render(SpriteBatch batch) {
 
+        TextureRegion reg = null;
+
+        float relX = 0;
+        float relY = 0;
+
+        reg = regEdge;
+        relX -= dimension.x/4;
+
+        batch.draw(reg.getTexture(), position.x+relX,position.y+relY, origin.x, origin.y,dimension.x/4,dimension.y,scale.x, scale.y,rotation,reg.getRegionX(),
+                reg.getRegionY(),reg.getRegionWidth(), reg.getRegionHeight(), false, false);
+
+        relX = 0;
+        reg = regMiddle;
+
+        for(int i = 0; i< length; i++){
+            batch.draw(reg.getTexture(), position.x+relX,position.y+relY, origin.x, origin.y,dimension.x,dimension.y,scale.x, scale.y,rotation,reg.getRegionX(),
+                    reg.getRegionY(),reg.getRegionWidth(), reg.getRegionHeight(), false, false);
+            relX+=dimension.x;
+        }
+
+        reg = regEdge;
+
+        batch.draw(reg.getTexture(), position.x+relX,position.y+relY, origin.x+dimension.x/8, origin.y,dimension.x/4,dimension.y,scale.x, scale.y,rotation,reg.getRegionX(),
+                reg.getRegionY(),reg.getRegionWidth(), reg.getRegionHeight(), true, false);
     }
 
     public void setLength(int length) {
